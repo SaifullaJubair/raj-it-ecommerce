6ec41f1469375b5c37c2519f449bd4a349ad366a
Md Saifulla Jubair <101522775+SaifullaJubair@users.noreply.github.com>
2023-04-08 23:18:24 +0600

    responsive done

--- src/components/FlashSale/FlashSale.js
+++ src/components/FlashSale/FlashSale.js
@@ -5,26 +5,26 @@ import FlashSaleTitleSection from './FlashSaleTitleSection';
 const FlashSale = () => {
    return (
       <div className='max-w-[1920px] mx-auto'>
-         <div className='flex lg:mx-[60px]'>
+         <div className='flex md:flex-col lg:flex-col xl:flex-col 2xl-flex-row flex-col lg:mx-[60px] md:mx-7 mx-6'>
             <div>
                {/* flash sate title  */}
                <div className='flex items-center justify-between'>
                   <FlashSaleTitleSection></FlashSaleTitleSection>
                   <div className='flex items-center justify-center '>
                      <BsArrowLeftShort className='mr-2 text-2xl'></BsArrowLeftShort>
-                     <BsArrowRightCircleFill className=' text-xl'></BsArrowRightCircleFill>
+                     <BsArrowRightCircleFill className='mr-4 text-xl'></BsArrowRightCircleFill>
                   </div>
                </div>
                {/* flash sale card section  */}
-               <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mt-6 gap-3'>
+               <div className='grid grid-cols-1 justify-items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 mt-6 gap-8 '>
                   <FlashCard></FlashCard>
                   <FlashCard></FlashCard>
                   <FlashCard></FlashCard>
                   <FlashCard></FlashCard>
                   <FlashCard></FlashCard>
+                  <img src="https://i.ibb.co/5FDsVJP/adbanner.jpg" className='h-[516px]' alt="" />
                </div>
             </div>
-            <img src="https://i.ibb.co/5FDsVJP/adbanner.jpg" className='mx-2' alt="" />
          </div>
 
       </div>
